04d9377dd85a842b883edfd91c1fa360f088b55a
tcfabritius <tim.fabritius@skiff.com>
2023-07-04 11:36:47 +0300

    second commit

---
+++ src/app/details/details.page.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-details',
+  templateUrl: './details.page.html',
+  styleUrls: ['./details.page.scss'],
+})
+export class DetailsPage implements OnInit {
+
+  author:any
+  content:any
+  description:any
+  publishedAt:any
+  url:any
+  image:any
+  source:any
+  title:any
+
+  constructor(private router:Router, private activatedRoute:ActivatedRoute) { }
+
+  ngOnInit() {
+    this.author = this.activatedRoute.snapshot.queryParamMap.get('author')
+    this.content = this.activatedRoute.snapshot.queryParamMap.get('content')
+    this.description = this.activatedRoute.snapshot.queryParamMap.get('description')
+    this.publishedAt = this.activatedRoute.snapshot.queryParamMap.get('publishedAt')
+    this.source = this.activatedRoute.snapshot.queryParamMap.get('source')
+    this.title = this.activatedRoute.snapshot.queryParamMap.get('title')
+    this.url = this.activatedRoute.snapshot.queryParamMap.get('url')
+    this.image = this.activatedRoute.snapshot.queryParamMap.get('urlToImage')
+  }
+
+}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NewsArticlesService } from '../api/news-articles.service';
+import { Router, NavigationExtras } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -11,7 +12,7 @@ export class HomePage {
   selectedCategory:any = 'technology'
   topHeadlines:any = []
 
-  constructor(private articleService:NewsArticlesService) {
+  constructor(private articleService:NewsArticlesService, private router:Router) {
     articleService.getTopHeadlines().subscribe((results) => {
       this.topHeadlines.push(...results.articles)
     })
@@ -19,6 +20,22 @@ export class HomePage {
     articleService.getArticleByCategory().subscribe((results) => {
 
     })
+  }
+
+  getDetails(selectedArticle: { author: any; content: any; description: any; publishedAt: any; source: any; title: any; url: any; urlToImage: any; }){
+    const params: NavigationExtras = {
+      queryParams: {
+        'author': selectedArticle.author,
+        'content': selectedArticle.content,
+        'description': selectedArticle.description,
+        'publishedAt': selectedArticle.publishedAt,
+        'source': selectedArticle.source.name,
+        'title': selectedArticle.title,
+        'url': selectedArticle.url,
+        'urlToImage': selectedArticle.urlToImage
+      }
+    }
+    this.router.navigate(['/details'], params)
   }
 
 }
